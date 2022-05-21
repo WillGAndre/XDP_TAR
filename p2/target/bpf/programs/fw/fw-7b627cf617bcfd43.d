fbefd46eae32eec08bd2ca4270e58458eed35ddf
WillGAndre <xunga@mail.com>
2022-05-21 23:12:37 +0100

    Added mods

--- p2/target/bpf/programs/fw/fw-7b627cf617bcfd43.d
+++ p2/target/bpf/programs/fw/fw-7b627cf617bcfd43.d
@@ -1,8 +1,8 @@
-/home/will/Desktop/XDP_TAR/p2/target/bpf/programs/fw/fw-7b627cf617bcfd43.bc: src/fw/main.rs src/fw/config
+/home/will/Desktop/XDP_TAR/p2/target/bpf/programs/fw/fw-7b627cf617bcfd43.bc: src/fw/main.rs src/fw/test
 
-/home/will/Desktop/XDP_TAR/p2/target/bpf/programs/fw/fw-7b627cf617bcfd43: src/fw/main.rs src/fw/config
+/home/will/Desktop/XDP_TAR/p2/target/bpf/programs/fw/fw-7b627cf617bcfd43: src/fw/main.rs src/fw/test
 
-/home/will/Desktop/XDP_TAR/p2/target/bpf/programs/fw/fw-7b627cf617bcfd43.d: src/fw/main.rs src/fw/config
+/home/will/Desktop/XDP_TAR/p2/target/bpf/programs/fw/fw-7b627cf617bcfd43.d: src/fw/main.rs src/fw/test
 
 src/fw/main.rs:
-src/fw/config:
+src/fw/test:
