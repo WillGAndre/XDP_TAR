ed40f3c63d180eb86ee6f7686cdd82e0238857d7
WillGAndre <xunga@mail.com>
2022-05-28 18:15:12 +0100

    added fix to firewall

--- p2/src/fw/main.rs
+++ p2/src/fw/main.rs
@@ -2,10 +2,11 @@
 #![no_main]
 
 use core::fmt::Error;
-use core::str;
+use core::{str, mem};
 use core::str::*;
 use core::convert::TryInto;
 use cty::*;
+use redbpf_probes::xdp::DevMap;
 use redbpf_probes::xdp::prelude::*;
 use redbpf_probes::bindings::*;
 
@@ -13,17 +14,53 @@ program!(0xFFFFFFFE, "GPL");
 
 const XDP_PASS: XdpAction = XdpAction::Pass;
 const XDP_DROP: XdpAction = XdpAction::Drop;
+const XDP_REDIRECT: XdpAction = XdpAction::Redirect;
+const XDP_TX: XdpAction = XdpAction::Tx;
+
+// * --- *
+
+#[map]
+static mut map: DevMap = DevMap::with_max_entries(2);
 
 // * --- *
 
 // XDP/eBPF based IP-layer firewall to drop all UDP packets.
 #[xdp]
 pub fn xdp_ip_firewall(ctx: XdpContext) -> XdpResult {
+    let data = ctx.data_start();
+    let data_end = ctx.data_end();
+    let s_eth = mem::size_of::<*const ethhdr>();
+    let s_ip = mem::size_of::<*const iphdr>();
+
+    // Block malformed packets
+    if data + s_eth > data_end {
+        return Ok(XDP_DROP)
+    } else if data + s_eth + s_ip > data_end {
+        return Ok(XDP_DROP)
+    }
+    
+    /*
+    unsafe {
+        if let Ok(_) = map.redirect(2 as u32) {
+            return Ok(XDP_REDIRECT)
+        }
+    }
+    */
+
     // Blacklist ips
     let ips = include!("block-ip");
     if let Ok(ip_saddr) = get_ip_saddr(&ctx) {
         for ip in ips {
             if ip_saddr == ip.to_be() {
+                /*
+                unsafe {
+                    if let Ok(_) = map.redirect(1) {
+                        return Ok(XDP_REDIRECT)
+                    }
+                    // return Ok(bpf_redirect((*ctx_xdp_md).ingress_ifindex as u32, 0));
+                }
+                */
+                // return Ok(XDP_REDIRECT)
                 return Ok(XDP_DROP)
             }
         }  
@@ -68,12 +105,10 @@ pub fn xdp_ip_firewall(ctx: XdpContext) -> XdpResult {
 
 fn get_ip_protocol(ctx: &XdpContext) -> Result<u32, Error> {
     if let Ok(ip) = ctx.ip() {
-        // We need to make raw pointer into a u32 so `unsafe` is required.
         unsafe {
             return Ok((*ip).protocol as u32);
         }
     }
-    // Anything above `255` is reserved.
     return Ok(0x10000);
 }
 
